1de0faa71e99d9faeab494de2327014bdbaf2eb8
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-04-19 19:59:18 +0200

    Change Ocean Blue background color

--- app/themes.ts
+++ app/themes.ts
@@ -66,7 +66,7 @@ export const themes = [
     {
       id: "oceanBlue",
       name: "Ocean Blue",
-      bg: "#003fff",
+      bg: "#061dfd",
       fg: "#b7d4ff",
       fgMuted: "#b7d4ff99",
       accent: "#3ecfff",
